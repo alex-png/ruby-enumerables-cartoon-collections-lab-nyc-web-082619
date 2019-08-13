0b745ee30c1cec2a2cd8a5105e94354347c819dc
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-13 05:08:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ array.each_with_index do |el, i|
 end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
 end
 
 def long_planeteer_calls# code an argument here
